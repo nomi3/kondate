0ed18dd1fe130465361d95be268ef690657e61df
nomi3 <thisisnomi3@gmail.com>
2021-08-10 23:09:26 +0900

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,85 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <input v-model="url" placeholder="url here">
+    <button v-on:click="addItem">add url</button>
+    <table>
+      <tbody>
+        <tr v-for="(item, index) in items" :key="index">
+            <td>{{ item.message }}</td>
+            <td><button v-on:click="deleteItem(item)">delete</button></td>
+        </tr>
+      </tbody>
+    </table>
+    <button v-if="items.length" v-on:click="create">create kaimono list</button>
+    <table v-if="results.length">
+      <tbody>
+        <tr v-for="(result, index) in results" :key="index">
+            <td>{{ result.name }}</td>
+            <td>{{ result.amount }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data () {
+    return {
+      url: '',
+      items: [],
+      results: [],
+      mockList: [
+        {
+          name: 'じゃがいも',
+          amount: '2個'
+        },
+        {
+          name: 'にんじん',
+          amount: '2本'
+        }
+      ]
+    }
+  },
+  methods: {
+    addItem: function () {
+      if (this.url !== '') {
+        this.items.unshift({
+          message: this.url
+        })
+        this.url = ''
+      }
+    },
+    deleteItem: function (item) {
+      var index = this.items.indexOf(item)
+      this.items.splice(index, 1)
+    },
+    create: function () {
+      console.log('kondate!')
+      this.results = this.mockList
+    },
+    checkUrl: function () {
+      // 語頭が白ごはんかyoutubeなのを確認する
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
+h1, h2 {
+  font-weight: normal;
 }
 a {
   color: #42b983;
+}
+ul {
+  list-style-type: none;
+}
+
+table {
+  margin-left: auto;
+  margin-right: auto;
 }
 </style>
